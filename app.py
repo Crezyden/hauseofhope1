f2e67261bdff088b90b91a5cfa8be416b75f93ad
Crezydencik <69566707+Crezydencik@users.noreply.github.com>
2021-03-25 10:22:44 +0200

    1 save

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, url_for, request, jsonify
+from flask_mail import Mail, Message
+
+app =Flask(__name__)
+# app.config['MAILL_SERVER']='smtp.googlemail.com'
+# app.config['MAIL_PORT']= 465
+# app.config['MAIL_USE_SLL']= True
+# app.config['MAIL_debug']= False
+# app.config['MAIL_USERNAME']='deniss.kargins@gmail.com'
+# app.config['MAIL_PASSWORD']=''
+# app.config['MAIL_DEFAULT_SENDER']='deniss.kargins@gmail.com'
+# app.config['MAIL_MAX_EMAILS']= None
+# app.config['MAIL_ASCII_ATTA']= None
+
+# app.config.from_pyfile['config.cfg']
+# mail=Mail(app)  4
+
+@app.route("/")
+@app.route("/home", methods=['GET','POST'])
+def index():
+    return render_template("home.html")
+
+@app.route("/Whoweare")
+def about():
+    return render_template("whoweare.html")
+
+@app.route("/Hope21")
+def hope():
+    return render_template("hope21.html")
+
+@app.route("/Gallery")
+def gallery():
+    return render_template("gallery.html")
+
+@app.route("/Activity")
+def activity():
+    return render_template("activity.html")
+
+@app.route("/Contacts")
+def contacs():
+    return render_template("contacts.html")
+
+# @app.route("/forms", methods=['POST'])
+# def forms():
+
+        # """User sent an email request"""
+        # msg = Message("Feedback", recipients='deniss.kargins@gmail.com')
+        # msg.body = "You have received new feedback from {0} {1} <{2}>.\n\n {3}".format(
+        #     request.form['yourname'],
+        #     request.form['email'],
+        #     request.form['subject'],
+        #     request.form['mess'])
+        # try:
+        #     mail.send(msg)
+        #     msg = "We will respond as soon as possible."
+        #     category = "success"
+        # except Exception as err:
+        #     msg = str(err)
+        #     category = "danger"
+        #
+        # resp = {'feedback': msg, 'category': category}
+        # return make_response(jsonify(resp), 200)
+        #
+        # return render_template('contacts.html')
+
+    # name=request.form('youname')
+    # email=request.form('email')
+    # subject=request.form('subject')
+    # mess=request.form('mess')
+    # msg = Message("Hello", sender="hauseofhope@gmail.com", recipients=["deniss.kargins@gmail.com"])
+    # mail.send(msg)
+    # message="Вам отправлен запрос."
+    # server=smtplib.SMTP('smtp.gmmail.com', 587)
+    # server.starttls()
+if __name__ == '__main__':
+    app.run(debug=True  )
---
+++ static/returncall/retcal.js
@@ -0,0 +1,83 @@
+  $(document).ready(function(){
+    $("#sandmail").click(function(e){
+    var name =$("#name").val().trim();
+    var epasts=$("#email").val().trim();
+    var subject =$("#subject").val().trim();
+    var mass = $("#message").val().trim();
+  if(name ==""){
+          $("#errorname").text("Your name");
+          $('#errorname').removeClass('hidden');
+        }
+        else {
+          $("#errorname").text("");
+           $('#errorname').addClass('hidden');
+        }
+  if(subject==""){
+          $("#errormulic").text("*Email");
+          $('#errormulic').removeClass('hidden');
+        }
+        else {
+          $("#errormulic").text("");
+          $('#errormulic').addClass('hidden'); }
+  if(mass =="") {
+          $("#errorphone").text("*Телефон");
+          $('#errorphone').removeClass('hidden');
+                }
+        else {
+          $("#errorphone").text("");
+          $('#errorphone').addClass('hidden');
+       }
+if(epasts.split("@").length - 1 == 0|| epasts.split(".").length - 1 ==0){
+         $("#erroremail").text("*Введите е-mail");
+         $('#erroremail').removeClass('hidden');
+         epasts = "";
+       } else{
+         $("#erroremail").text("");
+         $('#erroremail').addClass('hidden');
+      }
+    if ((name !="") && (subject!="") && (mass!="") && (epasts!=""))
+        {
+       		$.ajax({
+              url: "static/returncall/mail.php",
+        		  type: "POST",
+              cahse:  false,
+              processData: false,
+              contentType: false,
+              data: {
+                "name": name,
+                "subject": subject,
+                "mass": mass,
+                "epasts": epasts
+              },
+              beforeSend: function(){
+              $('#sandmail').attr ("disabled", "disabled");
+              $(".retbutton").css('background-color','#FF0000');
+              },
+              success: function(data){
+                if (data) {
+                				$('#name').val ("");
+                				$('#email').val ("");
+                				$('#subject').val ("");
+                        $('#mass').val ("");
+                    		$('#sandmail').removeAttr("disabled");
+                        $(".retbutton").val(function (){
+                          $(".retbutton").css('background-color','#ff9700');
+                          $(".retbutton").css('color','#fff');
+                          $("#sandmail").html("Отправлено");
+                        });
+                        $(".retbutton").click(function (){
+                          $(".retbutton").css('background-color','#fff');
+                          $(".retbutton").css('color','#44b5c4');
+                          $("#sandmail").html("Получить");
+                        });
+                        $('.js-overlay-campaign').fadeOut();
+                    } else {
+                          alert("");
+                          $('#sandmail').removeAttr("disabled");
+                      }
+                  }
+      			}) ;
+          }
+      });
+      return false;
+  });
