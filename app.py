29e1f5141d15d0ec1aff6ee5f0e6de4fcceef122
Crezydencik <69566707+Crezydencik@users.noreply.github.com>
2021-03-27 22:01:39 +0200

    add 3

--- app.py
+++ app.py
@@ -73,6 +73,6 @@ def contacs():
     # server=smtplib.SMTP('smtp.gmmail.com', 587)
     # server.starttls()
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    # port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
     # app.run(debug=True  )
